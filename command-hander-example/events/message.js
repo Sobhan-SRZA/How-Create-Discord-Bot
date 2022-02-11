899e65588ba56a1789460edfb1498912acd1bccb
Sobhan.SRZA <90289153+Sobhan-SRZA@users.noreply.github.com>
2022-02-11 17:46:10 +0330

    Create message.js

---
+++ command-hander-example/events/message.js
@@ -0,0 +1,14 @@
+module.exports = async (client, message) => {
+    if (message.author.bot || message.channel.type === 'dm') return;
+
+    const prefix = "BOT_PREFIX";
+
+    if (message.content.indexOf(prefix) !== 0) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    const cmd = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command));
+
+    if (cmd) cmd.execute(client, message, args);
+};
