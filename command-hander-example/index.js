3509be67bf89c68d7d1a67cf14f8fbac2b7ea2e4
Sobhan.SRZA <90289153+Sobhan-SRZA@users.noreply.github.com>
2022-02-11 17:45:22 +0330

    Create index.js

---
+++ command-hander-example/index.js
@@ -0,0 +1,35 @@
+//===========================================================================================================//
+//consol
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client({ disableMentions: 'everyone' });  
+const prefix = "BOT_PREFIX";
+client.commands = new Discord.Collection();
+client.login("BOT_TOKEN");
+
+
+//===========================================================================================================//
+//Loading Commands
+fs.readdirSync('./commands').forEach(dirs => {
+    const commands = fs.readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
+    for (const file of commands) {
+        const command = require(`./commands/${dirs}/${file}`);
+        console.log(`Loading command ${file}`);
+        client.commands.set(command.name.toLowerCase(), command);
+    };
+});
+
+//===========================================================================================================//
+//Events
+const events = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of events) {
+    console.log(`Loading discord.js event ${file}`);
+    const event = require(`./events/${file}`);
+    client.on(file.split(".")[0], event.bind(null, client));
+};
+
+//===========================================================================================================//
+//Run Bot
+client.on("ready", () => {
+console.log(`${client.user.tag} Is Now Online :)`)
+})
